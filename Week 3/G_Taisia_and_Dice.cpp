10d7a0800fc15be506d212723ecbf84a2d13bf81
ratulhasan001 <ratulhasan2108@gmail.com>
2023-10-16 20:37:29 +0600

    week 3

--- Week 3/G_Taisia_and_Dice.cpp
+++ Week 3/G_Taisia_and_Dice.cpp
@@ -9,6 +9,13 @@ using ll = long long;
 #include "deBUG.h"
 #endif
 
+
+int total(vector<int>v) {
+    int sum = 0;
+    for(auto x : v) sum += x;
+    return sum;
+}
+
 int32_t main() {
 
     ios_base::sync_with_stdio(false);
@@ -17,12 +24,47 @@ int32_t main() {
     int T; 
     cin >> T;
     for(int testCase=1; testCase <= T; testCase++) {
-    
-        int n, s, r, cnt = 0, fnt = 0;
+
+        int n, s, r, cnt = 0, fnt = 0; 
         cin >> n >> s >> r;
-        vector<int>v(n);
-        v.push_back(s - r);
-        
+        vector<int>v;
+        int mx = s - r;
+        int baki = r / (n - 1);
+        v.push_back(mx);
+        for(int i = 0; i < n - 1; i++) {
+            v.push_back(baki);
+        }
+        debug(v);
+        ranges::sort(v);
+        if(total(v) < s) {
+            bool ok = false;
+            while(true) {
+                for(int i = 0; i < n - 1; i++) {
+                    v[i]++;
+                    if(total(v) == s) {
+                        ok = true;
+                        break;
+                    }
+                }   
+                if(ok) break;
+            }
+        }
+        else if(total(v) > s) {
+            bool ok = false;
+            while(true) {
+                for(int i = 0; i < n - 1; i++) {
+                    if(v[i] - 1 >= 2) v[i]--;
+                    if(total(v) == s) {
+                        ok = true;
+                        break;
+                    }
+                }
+                if(ok) break;
+            }
+        }
+        for(auto x : v) cout << x << ' ';
+        cout << '\n';
+
 
     }
     return 0;
